061d157c0d26a61cecf8881395947272111c87ea
Andreas Prang <a@pra.ng>
2020-07-03 00:09:19 +0200

    Initial Commit

---
+++ File Picker/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  File Picker
+//
+//  Created by Andreas Prang on 03.07.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ File Picker/File_PickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  File_PickerApp.swift
+//  File Picker
+//
+//  Created by Andreas Prang on 03.07.20.
+//
+
+import SwiftUI
+
+@main
+struct File_PickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
