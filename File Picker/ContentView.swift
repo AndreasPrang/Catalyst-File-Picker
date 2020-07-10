951f777fa634b0728016d179d6db21eb29a27c75
Andreas Prang <a@pra.ng>
2020-07-10 21:30:41 +0200

    Re-Indent

--- File Picker/ContentView.swift
+++ File Picker/ContentView.swift
@@ -7,38 +7,38 @@
 
 import SwiftUI
 
-	struct ContentView: View {
-		@ObservedObject var picker = DocumentPicker()
-		@State private var urlPick = ""
-
-		var body: some View {
-				HStack {
-					urlPickedFoRTextField()
-						.textFieldStyle(RoundedBorderTextFieldStyle())
-					Spacer()
-					Button(action: {
-						#if targetEnvironment(macCatalyst)
-						let viewController = UIApplication.shared.windows[0].rootViewController!
-						viewController.present(self.picker.viewController, animated: true)
-						self.picker.objectWillChange.send()
-						#endif
-						print("Hai premuto il pulsante per determinare il path della GeoFolder")
-					}) {
-						Image(systemName: "square.and.arrow.up")
-					}
-				}
-				.padding()
-		}
-
-		private func urlPickedFoRTextField() -> some View {
-			if picker.urlsPicked.count > 0 {
-				DispatchQueue.main.async {
-					self.urlPick = self.picker.urlsPicked[0].path
-				}
+struct ContentView: View {
+	@ObservedObject var picker = DocumentPicker()
+	@State private var urlPick = ""
+	
+	var body: some View {
+		HStack {
+			urlPickedFoRTextField()
+				.textFieldStyle(RoundedBorderTextFieldStyle())
+			Spacer()
+			Button(action: {
+				#if targetEnvironment(macCatalyst)
+				let viewController = UIApplication.shared.windows[0].rootViewController!
+				viewController.present(self.picker.viewController, animated: true)
+				self.picker.objectWillChange.send()
+				#endif
+				print("Hai premuto il pulsante per determinare il path della GeoFolder")
+			}) {
+				Image(systemName: "square.and.arrow.up")
 			}
-			return TextField("", text: $urlPick)
 		}
+		.padding()
 	}
+	
+	private func urlPickedFoRTextField() -> some View {
+		if picker.urlsPicked.count > 0 {
+			DispatchQueue.main.async {
+				self.urlPick = self.picker.urlsPicked[0].path
+			}
+		}
+		return TextField("", text: $urlPick)
+	}
+}
 
 struct ContentView_Previews: PreviewProvider {
 	static var previews: some View {
